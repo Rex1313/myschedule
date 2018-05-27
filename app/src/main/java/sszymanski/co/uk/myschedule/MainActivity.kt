8e6232158562865d18eb6f206acbb04abb3daef1
Sylwester Moniuszko-Szymanski <sylwester.moniuszko-szymanski@yotta.co.uk>
2018-05-27 13:01:10 +0100

    initial commit

---
+++ app/src/main/java/sszymanski/co/uk/myschedule/MainActivity.kt
@@ -0,0 +1,12 @@
+package sszymanski.co.uk.myschedule
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
